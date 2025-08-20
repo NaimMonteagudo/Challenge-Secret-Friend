54c7b38db775e70403f8e5fa17aef7ed92d79c34
nico <nicomonteagudo14@gmail.com>
2025-08-20 18:28:43 -0300

    Updat app.js

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
+const validName = (str) => /^[a-zA-Z\s]+$/.test(str);
+
 let friendsList = [];
+let showingFriends = document.getElementById('friendsList');
+let secretFriend = document.getElementById('result');
 
 function addFriend(){
     let friendName = document.getElementById('friend').value;
-    let copyFriend = friendName.toLowerCase();
-    let isItOnTheList = (element) => element.toLowerCase() === copyFriend;
-
+    let friendCopy = friendName.toLowerCase();
+    let isItOnTheList = (element) => element.toLowerCase() === friendCopy;
     if(friendsList.some(isItOnTheList)) alert("Your friend is already on the list");
-    else if(friendName != ''){
+    else if(validName(friendName)){
         friendsList.push(friendName);
         updateFriendList();
-        showFriend();
     }
+    friendName.innerHTML = "";
     return;
 }
 
 function updateFriendList(){
-    
-}
-
-function showFriend(){
-    let showing = document.getElementById('friendsList');
-    let li = document.createElement("li");
-    li.textContent = friendsList[friendsList.length-1];
-    showing.appendChild(li);
+    let container = document.getElementById("friendsList");
+    container.innerHTML = "";
+    friendsList.forEach(friend => {
+        let li = document.createElement("li");
+        li.textContent = friend;
+        container.appendChild(li);
+    });
     return;
 }
-
-
-addFriend();
-
 
 function drawFriend(){
-
-    showResults();
+    let randomPos = parseInt(Math.random()*friendsList.length);
+    let li2 = document.createElement("li2");
+    li2.textContent = `The secret friend is ${friendsList[randomPos]}`;
+    secretFriend.appendChild(li2);
+    restartGame();
     return;
 }
 
-
-
-
-
-
+function restartGame(){
+    friendsList = [];
+    showingFriends.innerHTML = "";
+}
